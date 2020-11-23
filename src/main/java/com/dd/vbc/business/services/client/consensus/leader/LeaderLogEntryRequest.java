fddf1e23007209d912bad2c738f93364d6378332
achrisgrimes@gmail.com <github@cKreb$#8173>
2020-11-23 09:32:43 -0500

    commented code out to test blockChainService

--- src/main/java/com/dd/vbc/business/services/client/consensus/leader/LeaderLogEntryRequest.java
+++ src/main/java/com/dd/vbc/business/services/client/consensus/leader/LeaderLogEntryRequest.java
@@ -73,16 +73,17 @@ public class LeaderLogEntryRequest implements ApplicationListener<LogEntryEvent>
                     entry.setLog(false);
                     entry.setCommit(true);
                     if(ConsensusState.getLeaderCommitList().get(entry.getIndex().intValue())==Boolean.FALSE) {
-                        Consumer<AppendEntry> oSuccess = (AppendEntry appendEntry) -> {
-                            ConsensusState.getLeaderCommitList().set(appendEntry.getIndex().intValue(), Boolean.TRUE);
-                            appendEntry.setServer(ConsensusServer.getServerInstance());
-                            applicationEventPublisher.publishEvent(new CommitEntryEvent(appendEntry));
-                        };
-                        Consumer<Throwable> oError = Throwable::getMessage;
-                        Runnable oCompletion = () -> { if(log.isDebugEnabled()) log.debug("onApplicationEvent: Message Completed"); };
-
-                        blockChainService.followerCommitEntryResponse(entry).
-                            subscribe(oSuccess, oError, oCompletion);
+                        log.debug("Majority of followers has Logged Entry");
+//                        Consumer<AppendEntry> oSuccess = (AppendEntry appendEntry) -> {
+//                            ConsensusState.getLeaderCommitList().set(appendEntry.getIndex().intValue(), Boolean.TRUE);
+//                            appendEntry.setServer(ConsensusServer.getServerInstance());
+//                            applicationEventPublisher.publishEvent(new CommitEntryEvent(appendEntry));
+//                        };
+//                        Consumer<Throwable> oError = Throwable::getMessage;
+//                        Runnable oCompletion = () -> { if(log.isDebugEnabled()) log.debug("onApplicationEvent: Message Completed"); };
+//
+//                        blockChainService.followerCommitEntryResponse(entry).
+//                            subscribe(oSuccess, oError, oCompletion);
                     }
                 } else {
                     log.debug("majority followers not logged");
